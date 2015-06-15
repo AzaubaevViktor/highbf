a9c59a669955b86af5d883ef6fe7a35b4c963589
ktulhy <sarvash.inc@gmail.com>
2015-06-16 03:39:22 +0600

    Использован модуль shlex для лексера

--- compilers/compilers_utils/lexer/__init__.py
+++ compilers/compilers_utils/lexer/__init__.py
@@ -1,3 +1,3 @@
 __author__ = 'ktulhy'
 
-from compilers.compilers_utils.lexer.lexer import Lexer, Token
+from .lexer import Token, Lexer
--- compilers/compilers_utils/lexer/lexer.py
+++ compilers/compilers_utils/lexer/lexer.py
@@ -1,138 +1,30 @@
 __author__ = 'ktulhy'
 
-class FilePos:
-    def __init__(self):
-        self._line = 1
-        self._pos = 1
-        self._prev_pos = 0
-        self.backward = 0
-
-    def next(self):
-        if self.backward != 0:
-            self.backward -= 1
-        else:
-            self._pos += 1
-
-    def newline(self):
-        self._line += 1
-        self._prev_pos = self._pos
-        self._pos = 0
-
-    def prev(self):
-        self.backward += 1
-
-    @property
-    def line(self):
-        if self.backward == 0:
-            return self._line
-        else:
-            if self._pos - self.backward <= 0:
-                return self._line - 1
-            else:
-                return self._line
-
-    @property
-    def pos(self):
-        if self.backward == 0:
-            return self._pos
-        else:
-            if self._pos - self.backward <= 0:
-                return self._prev_pos - (self._pos - self.backward)
-            else:
-                return self._pos
+import shlex
 
 
 class Token:
-    def __init__(self, s, line, pos):
-        self.s = s.rstrip().lstrip()
+    def __init__(self, line=0, text=None, token_type=None):
         self.line = line
-        self.pos = pos
-        self.type = None
+        self.text = text
+        self.token_type = token_type
 
-    def __str__(self):
-        return "[{}:{}:'{}']".format(self.line, self.pos, self.s)
+    def typing(self, func):
+        self.token_type = func(self)
 
-def _none_typing(token):
-    return None
+class Lexer(shlex.shlex):
+    typing = lambda x: None
+    prev_lineno = 0
 
-class Lexer:
-    def __init__(self, lines, terminals=" \t", non_terminals="\n"):
-        self.stream = iter("".join(lines))
-        self.is_stream_end = False
-        self.terminals = terminals
-        self.non_terminals = non_terminals
-        self.comments = "#"
-        self.deferred = None
-        self._pos = FilePos()
-        self.token_queue = []
-        self._is_commented = False
-        self.typing = _none_typing
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
 
     def get_token(self):
-        if len(self.token_queue) == 0:
-            self._next()
+        lineno = super().lineno
+        if self.prev_lineno != lineno:
+            return Token(self.prev_lineno, "\n")
 
-        return self.token_queue.pop()
+        token = Token(super().lineno, super().get_token())
+        token.typing(self.typing)
+        return token
 
-    def push_token(self, token_str, line=-1, pos=-1):
-        self.token_queue.append(Token(token_str, line, pos))
-
-    def _next(self):
-        token_line = self._pos.line
-        token_pos = self._pos.pos
-        token_str = ""
-
-        while 1:
-            if self.deferred:
-                token_str = self.deferred
-                self.deferred = None
-                self._pos.next()
-
-            ch = ""
-
-            try:
-                ch = next(self.stream)
-            except StopIteration:
-                self.is_stream_end = True
-                break
-
-            if ch == '\n':
-                self._pos.newline()
-
-            self._pos.next()
-
-            if self._is_commented:
-                if self._is_commented != self._pos.line:
-                    self._is_commented = False
-                else:
-                    continue
-
-            if ch in self.terminals:
-                if len(token_str) != 0:
-                    break
-                else:
-                    token_pos = self._pos.pos
-                    token_line = self._pos.line
-            elif ch in self.non_terminals:
-                if len(token_str) != 0:
-                    self._pos.prev()
-                    self.deferred = ch
-                    break
-                else:
-                    token_str = ch
-                    break
-            elif ch in self.comments:
-                self._is_commented = self._pos.line
-                continue
-            else:
-                token_str += ch
-
-        if len(token_str) != 0:
-            tok = Token(token_str, token_line, token_pos)
-            tok.type = self.typing(tok)
-            self.token_queue.append(tok)
-        else:
-            self.is_stream_end = True
-
-        if self.is_stream_end:
-            self.token_queue.append(None)
